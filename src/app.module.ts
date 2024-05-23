9b50ba48f3611c7e4e22d186286403170f3cc5b7
Ismail <ismail.salikhodjaev@toptal.com>
2024-05-23 19:25:18 +0500

    add module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './users/users.service';
+import { SkinportService } from './skinport/skinport.service';
+import { UserController } from './users/users.controller';
+import { SkinportController } from './skinport/skinport.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule],
+  controllers: [AppController, UserController, SkinportController],
+  providers: [AppService, UserService, SkinportService],
 })
 export class AppModule {}
